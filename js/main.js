c0af0fd40c1ee153228954781ec90d05457283d2
Tarek Mohamed <109057697+TAREKPOPTEKO@users.noreply.github.com>
2023-10-17 05:39:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var ser = document.getElementById("ser");
+var btnSer = document.getElementById("s");
+s.addEventListener("click", function () {
+  ser.style.display = "block";
+});
+var x = document.getElementById("x");
+x.addEventListener("click", function () {
+  ser.style.display = "none";
+});
